b287c5c9be133cacb0f3effe0777257616c66a8e
АДМИН <@DESKTOP-S9U5IDE>
2024-11-06 21:30:19 +0300

    Добавьте файлы проекта.

---
+++ PracticalWorkThree_girldooll/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите три числа: a,b,c");
+Console.Write("a= ");
+double a =Convert.ToDouble(Console.ReadLine());
+Console.Write("b= ");
+double b=Convert.ToDouble(Console.ReadLine());
+Console.Write("c= ");
+double c =Convert.ToDouble(Console.ReadLine());
+if (a + b + c == 0)
+{
+    Console.WriteLine("Сумма a+b+c=0");
+    return;
+}
+double y = (a * a + b * b + c * c) / (a + b + c);
+Console.WriteLine($"y= {y}");
+double e = Math.Exp(y);
+Console.WriteLine($"ближайшая экспонента к y={e}");
+
+
